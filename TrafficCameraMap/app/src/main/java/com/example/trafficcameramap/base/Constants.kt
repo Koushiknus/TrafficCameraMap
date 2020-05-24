1f49045e592a5cee20abe0feab68404b3ac7e780
koushik <koushik>
2020-05-24 23:26:49 +0800

    Update : Date Format dynamic added

--- TrafficCameraMap/app/src/main/java/com/example/trafficcameramap/base/Constants.kt
+++ TrafficCameraMap/app/src/main/java/com/example/trafficcameramap/base/Constants.kt
@@ -3,6 +3,7 @@ package com.example.trafficcameramap.base
 object Constants {
 
     val TIME_OUT = 60L
-    val DATE_TIME = "YYYY-MM-DD[T]HH:mm:ss (SGT)"
+    val DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss"
+    val TIME_ZONE = "UTC"
     val BASE_URL = "https://api.data.gov.sg/v1/transport/"
 }
--- TrafficCameraMap/app/src/main/java/com/example/trafficcameramap/ui/MapViewModel.kt
+++ TrafficCameraMap/app/src/main/java/com/example/trafficcameramap/ui/MapViewModel.kt
@@ -4,9 +4,13 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.example.trafficcameramap.base.BaseViewModel
+import com.example.trafficcameramap.base.Constants
 import com.example.trafficcameramap.network.ApiMethods
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
+
 
 class MapViewModel (application: Application) : BaseViewModel(application){
     @set: Inject
@@ -19,9 +23,16 @@ class MapViewModel (application: Application) : BaseViewModel(application){
     fun getTrafficImages(dateTime: String){
 
         viewModelScope.launch {
-            val result = mMapViewRepository?.getTrafficImages("2020-05-24T01:01:01")
+            val result = mMapViewRepository?.getTrafficImages(getCurrentDate())
             Log.v("ResultReceived",result.toString())
         }
+    }
+
+   private fun getCurrentDate(): String {
+        val dateFormat = SimpleDateFormat(Constants.DATE_TIME_FORMAT)
+        dateFormat.setTimeZone(TimeZone.getTimeZone(Constants.TIME_ZONE))
+        val today = Calendar.getInstance().time
+        return dateFormat.format(today)
     }
 
 }
